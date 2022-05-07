300c3dd572e4be28fd9e9b7d29b82c2b2f0b9fe1
jungwoo <hanjungwoo1@naver.com>
2022-05-07 20:29:29 +0900

    django

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from pybo import views
+
 urlpatterns = [
+    path('pybo/', include('pybo.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
