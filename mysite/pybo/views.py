ca86698a6fd654bb24811c4deb0ad7a6b469559d
jungwoo <hanjungwoo1@naver.com>
2022-06-19 17:26:34 +0900

    django

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
-from .models import Question
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Question, Answer
 
 
 
@@ -14,3 +15,9 @@ def detail(request, question_id):
     context = {'question': quest}
     return render(request, 'pybo/question_detail.html', context)
 
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    #question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
+    answer.save()
+    return redirect('pybo:detail', question_id=question.id)
